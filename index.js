2b3530e8c4d917834ae0d30fc5d749463d8a5457
Merve Gurler <65897975+MerveGurler@users.noreply.github.com>
2020-06-06 12:55:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-class User {
-    constructor(name, mail, password) {
-        this.name = name
-        this.mail = mail
-        this.password = password  
-    }
-
-    follow(user) {
-        console.log(this.name + " " + 'is following you');
-    } 
-}
-    
-class Auther extends User {
-    constructor(blogposts) {
-        this.blogposts = ['0', '1'];  
-    }  
-}
-
-class Posts {
-    constructor (text, auther){
-        this.text = text
-        this.auther = auther
-    }
-}
+const User = require('./user')
+const Blogpost = require('./blogpost')
 
 const merve = new User('Merve') 
 const mehmet = new User('Mehmet')
 
 merve.follow(mehmet)
+
+const mine = new Author('Mine')
+const blogPost = new Blogpost('Minenin blog post yazisi', mine);
+
+mine.write(mine, blogPost)
