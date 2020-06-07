55a164ab48b0dcfb50b327d730465dceb472e14c
Merve Gurler <65897975+MerveGurler@users.noreply.github.com>
2020-06-07 16:33:05 +0300

    Create place.js

---
+++ place.js
@@ -0,0 +1,9 @@
+class Place {
+    constructor (placename, location, star) {
+        this.placename = placename
+        this.location = location
+        this.star = star
+    }
+}
+
+module.exports = Place
