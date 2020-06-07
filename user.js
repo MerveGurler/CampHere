c4998daedd880e69c495016a7a6555f97e00a6fb
Merve Gurler <65897975+MerveGurler@users.noreply.github.com>
2020-06-07 16:32:13 +0300

    Update user.js

--- user.js
+++ user.js
@@ -1,24 +1,43 @@
 class User {
-    constructor(name, mail, password) {
+    constructor(name, mail, password, blogposts, placenames) {
         this.name = name
         this.mail = mail
-        this.password = password  
+        this.password = password 
+        this.blogposts = blogposts 
+        this.placenames = placenames
     }
 
-    follow (user) {
+    follow(user) {
         console.log(this.name + " " + 'is following' + " " + user.name)
     }
+
+    comment(blogpost){
+        this.blogposts.push(blogpost)
+        console.log(this.name + ' ' + 'commented at date')
+    }
+
+    rate(placename) {
+        console.log(this.name + " " + 'give' + " " + this.star + ' ' + this.placename)
+    }
+
+    like(placename, blogpost) {
+        console.log(this.name + " " + 'likes' + " " + this.placename)
+        console.log(this.name + " " + 'likes' + " " + this.blogpost)
+    }
+
 }
 
 class Author extends User {
-    constructor(name, mail, password, blogposts) {
+    constructor(name, mail, password) {
         super(name, mail, password)
         this.blogposts = []    
     }
 
     write(blogpost) {
         this.blogposts.push(blogpost)
+        console.log(this.name + ' ' + 'wrote' + ' ' + this.blogposts)
     }
 } 
 
-module.export = User
+module.exports = User
+module.exports = Author
