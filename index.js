eaaa71225dfa8ee6280f989f5bb9fd4f14d67f7b
Merve Gurler <65897975+MerveGurler@users.noreply.github.com>
2020-06-07 16:31:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const User = require('./user')
+const Author = require('./user')
 const Blogpost = require('./blogpost')
+const Place = require('./place')
 
 const merve = new User('Merve') 
 const mehmet = new User('Mehmet')
@@ -7,6 +9,15 @@ const mehmet = new User('Mehmet')
 merve.follow(mehmet)
 
 const mine = new Author('Mine')
-const blogPost = new Blogpost('Minenin blog post yazisi', mine);
+const blogPost = new Blogpost('Essay by Mine', 'mine')
+const placeName = new Place ('Red Tent Camping', 'Australia', '5 stars')
 
-mine.write(mine, blogPost)
+mine.write(blogPost)
+
+merve.comment(blogPost)
+
+merve.rate(blogPost)
+
+merve.like(placeName)
+
+merve.like(blogPost)
