d6eb36efe317231076059da9a5adf3bd73a7dc58
Merve Gurler <65897975+MerveGurler@users.noreply.github.com>
2020-06-27 16:17:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 class Camper {
     constructor (name) {
         this.name = name
@@ -10,13 +9,32 @@ class Camper {
     follow(camper) {
         this.followedCampers.push(camper)  
     }
-
 }
+
+class Blogpost {
+    constructor (writing, author) {
+        this.writing = writing
+        this.author = author
+    }
+}
+
 class Author extends Camper {
     constructor (name) {
         super(name)
-        this.blogPost = []
+        this.blogPosts = []
     }
+    write(post) {
+        const post = new Blogpost(writing, author)
+        this.blogPosts.push(post)
+        return post
+    }
+}
+
+//follow 
+
+/*function printFollowingHistory(camper) {
+    camper.followedCampers.forEach(followedCampers =>
+    console.log(`${camper.name} is following ${followedCampers.name}.`))
 }
 
 const aslan = new Camper('Aslan')
@@ -25,15 +43,25 @@ const ata = new Camper('Ata')
 
 banu.follow(aslan)
 banu.follow(ata)
-
-function printFollowing(camper) {
-    console.log(`${camper.name} is following ${camper.followedCampers.name}.`)
-}
-
-function printFollowingHistory(camper) {
-    this.followedCampers.forEach(printFollowing)
-}
+ata.follow(banu)
 
 printFollowingHistory(banu)
+printFollowingHistory(ata) */
 
+//write
 
+const evren = new Author('Evren')
+
+const post1 = evren.write('Camping at Night', 'Evren')
+const post2 = evren.write('Travel Guide')
+const post3 = evren.write('Surviving Class')
+
+function printWritingBlogPost(blogPost){
+    console.log(`${blogPost.author.name} wrote ${blogPost.writing}`)
+}
+
+printWritingBlogPost(post1)
+printWritingBlogPost(post2)
+printWritingBlogPost(post3)
+
+//console.log(`${evren.name} has ${evren.blogPosts.length} post(s).`)
