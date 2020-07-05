a2c5f245c2e1440a244a5ce5a67e0c913c674709
Merve Gurler <65897975+MerveGurler@users.noreply.github.com>
2020-07-05 11:02:20 +0300

    Update index.js

--- database/index.js
+++ database/index.js
@@ -2,4 +2,4 @@ const camperDatabase = require('./camper-database')
 const authorDatabase = require('./author-database')
 const blogPostDatabase = require('./blogpost-database')
 
-module.export = {camperDatabase, blogPostDatabase, authorDatabase}
+module.exports = {camperDatabase, blogPostDatabase, authorDatabase}
