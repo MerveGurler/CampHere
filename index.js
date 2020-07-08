afe4e85c4abd5e4c0b9478d454c411ff02c944ab
Merve Gurler <65897975+MerveGurler@users.noreply.github.com>
2020-07-08 18:17:04 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-const camperDatabase = require('./camper-database')
-const authorDatabase = require('./author-database')
-const blogPostDatabase = require('./blogpost-database')
+const {camperDatabase, authorDatabase, blogPostDatabase} = require('./database')
+const printBlogPostHistory = require('./lib/print-blogpost-history')
+const printFollowingHistory = require('./lib/print-following-history')
+const {Camper, Author} = require('./model/camper')
+const Blogpost = require('./model/blogpost')
 
-module.exports = {camperDatabase, blogPostDatabase, authorDatabase}
+/*
+const ata = new Camper(undefined, 'Ata')
+const banu = new Camper(undefined, 'Banu')
+const evren = new Author(undefined, 'Evren')
+
+ata.follow(banu)
+ata.follow(evren)
+
+printFollowingHistory(ata)
+
+const post1 = evren.write('Camping at Night', 'Evren')
+const post2 = evren.write('Travel Guide')
+const post3 = evren.write('Surviving Class')
+
+printBlogPostHistory(evren)
+*/
+
+const ata = Camper.create({id: undefined, name: 'Ata'})
+const banu = Camper.create({id: undefined, name: 'Banu'})
+const evren = Author.create({id: undefined, name: 'Evren'})
+
+camperDatabase.save([ata, banu])
+authorDatabase.save([evren])
+
+/*
+const post1 = Blogpost.create({text: 'Camping at Night', author: 'Evren'})
+const post2 = Blogpost.create({text: 'Travel Guide', author: 'Evren'})
+const post3 = Blogpost.create({text: 'Surviving Class', author: 'Evren'})
+
+blogPostDatabase.save('blogposts', [post1, post2, post3])
+*/
+
+const campers = camperDatabase.load()
+campers.forEach(printFollowingHistory)
+
+const authors = authorDatabase.load()
+authors.forEach(printFollowingHistory)
+
+const blogposts = blogPostDatabase.load()
+blogposts.forEach(printBlogPostHistory)
+
+
+//const ata = camperDatabase.findBy('name', 'Ata')
+//const evren = authorDatabase.findByName('Evren')
+
+
+//camperDatabase.update(ata)
+//authorDatabase.update(evren)
+
+
+//console.log(camperDatabase.findBy('name','Ata'))
+//console.log(authorDatabase.findByName('Evren'))
