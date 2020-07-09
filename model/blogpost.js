b6924732a1a37114bb4aeafc365de83f401c6e1f
Merve Gurler <65897975+MerveGurler@users.noreply.github.com>
2020-07-09 16:08:12 +0300

    Add files via upload

--- model/blogpost.js
+++ model/blogpost.js
@@ -3,6 +3,10 @@ class Blogpost {
         this.text = text
         this.author = author
     }
+
+    static create({text, author}) {
+        return new Blogpost(text, author)
+    }
 }
 
 module.exports = Blogpost
